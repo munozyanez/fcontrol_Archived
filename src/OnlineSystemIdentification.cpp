5bcce02bba93346b4bcbf9dcab924f62ea831838
munozyanez <munoz.yanez@gmail.com>
2019-03-20 18:41:40 +0100

    modificado:     src/OnlineSystemIdentification.cpp 	modificado:     src/OnlineSystemIdentification.h

--- src/OnlineSystemIdentification.cpp
+++ src/OnlineSystemIdentification.cpp
@@ -3,25 +3,56 @@
 OnlineSystemIdentification::OnlineSystemIdentification()
 {
 
-    ff=0.98;
-
-    P=P.Random();
-    phi=phi.setOnes();
-    L=L.setZero();
-    phi=phi.Random();
-
+    //Default params: order 1, forgetting factor 0.98
+    OnlineSystemIdentification(1,0.98);
 
 }
 
-OnlineSystemIdentification::OnlineSystemIdentification(double new_ff)
+OnlineSystemIdentification::OnlineSystemIdentification(long new_numOrder, long new_denOrder, double new_ff)
 {
-    ff=0.98;
+    ff=new_ff;
 
-    P=P.Random();
-    phi=phi.setOnes();
-    L=L.setZero();
-    phi=phi.Random();
-    OnlineSystemIdentification();
+    denOrder= new_denOrder;
+    numOrder= new_numOrder;
+
+    if(denOrder < numOrder)
+    {
+        numOrder = denOrder;
+        cout << "Only causal systems!!! Reducing numerator to order: " << numOrder << endl;
+    } //now it is causal, numOrder <= denOrder
+
+    //share orders available, allowing den use numerator unused orders.
+    if( denOrder + numOrder > rlms_N)
+    {
+        if (numOrder < rlms_N/2) //Give num orders to den.
+        {
+        denOrder = rlms_N - numOrder;
+        cout << "Maximum order reached. Reducing orders to: num = " << numOrder << ", den =" << denOrder << endl;
+        }
+        else //The only possibility is to reduce both to maximum size.
+        {
+            numOrder = rlms_N/2;
+            denOrder = numOrder;
+            cout << "Maximum order reached. Reducing orders to: num = " << numOrder << ", den = " << denOrder << endl;
+
+
+        }
+    }
+
+    order = numOrder + denOrder;
+
+//    P.resize(order,NoChange); //no longer needed
+    P=P.Random(order,order);
+    cout << "--> Initial P <--" << endl << P << endl;
+//    phi.resize(order,order); //no longer needed
+    phi=phi.Random(order,1);
+    cout << "--> Initial phi <--" << endl << phi << endl;
+//    L.resize(order,NoChange);//no longer needed
+    L=L.setZero(order,1);
+    cout << "--> Initial L <--" << endl << L  << endl;
+
+    th=th.setZero(order,1);
+    cout << "--> Initial th <--" << endl << th << endl << "--------------order : " << order << endl;
 
 
 }
@@ -29,32 +60,32 @@ OnlineSystemIdentification::OnlineSystemIdentification(double new_ff)
 long OnlineSystemIdentification::UpdateSystem(double input, double output)
 {
 
-
+ti++;
     //move all phi input data one position backwards to have inputs from actual to last needed past values.
-    for (int i=rlms_z-1; i>0; i--) //Leave the first place empty for current input value
+    for (int i=numOrder-1; i>0; i--) //Leave the first place empty for current input value
     {
         //input indexes can start from zero to include actual input??
-        phi(i+rlms_p) = phi(i+rlms_p-1);
+        phi(i+denOrder) = phi(i+denOrder-1);
     }
-    phi(0+rlms_p)=input;
+    phi(0+denOrder)=input;
 
     //        cout << "b0 " << (out[ti]-phi.transpose()*th)/in[ti] << ", at step: " <<  ti << endl;
-
     //        cout << "phi: " << phi.transpose() << endl;
+
     L = (P*phi)/(ff+(phi.transpose()*P*phi));
-    //        cout << "L " << L << ", at step: " <<  ti << endl;
+//    cout << "L " << L.transpose() << ", at step: " <<  ti << endl;
 
     P=(1/ff)*(P-L*phi.transpose()*P);
-    //        cout << "P " << P << ", at step: " <<  ti << endl;
+//    cout << "P " << P << ", at step: " <<  ti << endl;
 
     err=output-phi.transpose()*th;
-    //        cout << "Error preditcion " << err << ", at step: " <<  ti << endl;
+//    cout << "Error preditcion " << err << ", at step: " <<  ti << endl;
 
     th=th+(L*err);
-    cout << "Params " << th.transpose() << endl;
+//    cout << "Params " << th.transpose() << endl;
 
     //move all phi output data one position backwards for the next iteration
-    for (int i=rlms_p-1; i>0; i--)
+    for (int i=denOrder-1; i>0; i--)
     {
         phi(i) = phi(i-1);
     }
@@ -63,4 +94,31 @@ long OnlineSystemIdentification::UpdateSystem(double input, double output)
 
     return 0;
 
+}
+
+long OnlineSystemIdentification::GetZTransferFunction(vector<double> &num, vector<double> &den)
+{
+
+    for (int i=0; i<numOrder; i++)
+    {
+        num[i]=th(i);
+//        cout << "num i= " << i << " num: " << num[i] << " th: " << th(i)<<endl;
+    }
+
+    for (int i=0; i<denOrder; i++)
+    {
+        den[i]=th(i+numOrder);
+//        cout << "den i= " << i << " den: " << den[i] << " th: " << th(i+numOrder)<<endl;
+    }
+    return 0;
+}
+
+vector<double> OnlineSystemIdentification::GetParamsVector()
+{
+    vector<double> params(order);
+    for (int i=0; i<order; i++)
+    {
+        params[i]=th(i);
+    }
+    return params;
 }
--- src/OnlineSystemIdentification.h
+++ src/OnlineSystemIdentification.h
@@ -4,7 +4,8 @@
 
 #define rlms_z (1)                //Number of input past values including current input
 #define rlms_p 2					//Number of output past values
-#define rlms_N (rlms_z+rlms_p)      //phi dimension
+//#define rlms_N (rlms_z+rlms_p)      //phi dimension
+#define rlms_N (32)      //phi dimension More order than that will throw assertion on eigen
 
 #include <vector>
 #undef Success //as in http://eigen.tuxfamily.org/bz/show_bug.cgi?id=253
@@ -21,18 +22,29 @@ class OnlineSystemIdentification
 {
 public:
     OnlineSystemIdentification();
+    OnlineSystemIdentification(long new_numOrder, long new_denOrder, double new_ff = 0.98);
     long UpdateSystem(double input, double output);
+    long GetZTransferFunction(vector<double>& num,vector<double>& den);
+    vector<double> GetParamsVector();
 
-    OnlineSystemIdentification(double new_ff);
 private:
 
-    double ff; //forgetting factor
+    long numOrder,denOrder, order;
 
-    Matrix<double,rlms_N,rlms_N> P;
-    Matrix<double,rlms_N,1> phi;
-    Matrix<double,rlms_N,1> L;
-    Matrix<double,rlms_N,1> th;
+//    Matrix<double,Dynamic,Dynamic> P;
+//    Matrix<double,Dynamic,1> phi;
+//    Matrix<double,Dynamic,1> L;
+//    Matrix<double,Dynamic,1> th;
+
+//    Matrix<double, 1, 1, 0, 1, 1> ff; //forgetting factor
+    double ff;
+    Matrix<double, Dynamic, Dynamic, 0, rlms_N, rlms_N> P; //max order rlms_N #defined
+    Matrix<double, Dynamic, 1, 0, rlms_N, 1> phi; //max order rlms_N
+    Matrix<double, Dynamic, 1, 0, rlms_N, 1>  L;//max order rlms_N
+    Matrix<double, Dynamic, 1, 0, rlms_N, 1> th;//max order rlms_N
+
     double err;
+    double ti; //time index
 
     vector<double> oldOuts, oldIns;
 
